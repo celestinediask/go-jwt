6ec2f2ecf1ad58910d0a3d29ceba8ce0f4243a6f
Celestine <celestinediask@gmail.com>
2024-04-06 18:13:14 +0530

    login with cookie

--- controllers/user-controller.go
+++ controllers/user-controller.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/celestinediask/go-jwt/initializers"
 	"github.com/celestinediask/go-jwt/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -51,4 +54,70 @@ func Signup(c *gin.Context) {
 
 	// Respond
 	c.JSON(http.StatusOK, gin.H{})
+}
+
+func Login(c *gin.Context) {
+	// Get email and pass of req body
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return
+	}
+
+	// Look up requested user
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "User lookup, Invalid email or password",
+		})
+
+		return
+	}
+
+	// Compare send in pass with saved user pass hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Compare password, Invalid email or password",
+		})
+
+		return
+	}
+
+	// Generate a jwt token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create token",
+		})
+	}
+
+	// send it back
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{})
+}
+
+func Validate(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Im logged in",
+	})
 }
--- main.go
+++ main.go
@@ -15,5 +15,7 @@ func init() {
 func main() {
 	r := gin.Default()
 	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", controllers.Validate)
 	r.Run()
 }
